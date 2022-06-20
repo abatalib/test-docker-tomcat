5afab15996aecd6074346183b2f2c427fc769ff3
Abatalib Mohammed <abatalib@gmail.com>
2022-06-20 13:16:51 +0000

    2nd commit

--- src/main/java/com/abatalib/test/Api.java
+++ src/main/java/com/abatalib/test/Api.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Api {
     @GetMapping("/list")
     public String getList(){
-        return "hello";
+        return "hello world";
     }
 }
